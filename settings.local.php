47a17457d5295ce0c5a3af66cc8275ced6803881
Jorge Martinez <jorgemartinez@jorgemartinez.ethology.local>
2016-09-12 10:30:06 -0700

    adding local settings for drupal

---
+++ settings.local.php
@@ -0,0 +1,19 @@
+<?php
+$databases = array (
+  'default' =>
+    array (
+      'default' =>
+        array (
+          'database' => 'asu_ipe_drupal_web',
+          'username' => 'root',
+          'password' => 'Ipe4u',
+          'host' => 'localhost',
+          'port' => '',
+          'driver' => 'mysql',
+          'prefix' => '',
+        ),
+    )
+);
+
+ini_set('memory_limit', '1024M');
+set_time_limit (0);
